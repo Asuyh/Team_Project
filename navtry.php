20f46cedc946fdb9e69dfda1cdb86ad9b4acd896
Ayush KC <107458506+Asuyh@users.noreply.github.com>
2024-05-26 08:29:08 +0545

    navigation tryout

---
+++ navtry.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navbar</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+        .navbar {
+            background-color: #333;
+            color: white;
+            padding: 10px 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .nav-links {
+            display: flex;
+            list-style: none;
+        }
+        .nav-links a {
+            text-decoration: none;
+            color: white;
+            padding: 10px;
+            display: block;
+        }
+        .search-container {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            flex-grow: 1;
+        }
+        .search-container input[type="text"] {
+            padding: 8px;
+            margin-right: 10px;
+            width: 200px;
+        }
+        .logo img {
+            height: 50px;
+        }
+        .profile-image img {
+            border-radius: 50%;
+            height: 40px;
+            width: 40px;
+        }
+        .profile-menu {
+            position: relative;
+        }
+        .profile-menu ul {
+            position: absolute;
+            background-color: #555;
+            list-style: none;
+            padding: 10px;
+            display: none;
+            right: 0;
+        }
+        .profile-menu:hover ul {
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="navbar primary-navbar">
+        <div class="logo">
+            <a href="index.php">
+                <img src="./assets/images/logo-01.png" />
+            </a>
+        </div>
+        <div class="nav-links">
+            <a href="index.php">Home</a>
+            <a href="products.php">Products</a>
+            <a href="deals.php">Deals</a>
+        </div>
+        <div class="search-container">
+            <input type="text" placeholder="Search products">
+            <button>Search</button>
+        </div>
+        <div class="profile-menu">
+            <a href="profile.php">
+                <div class="profile-image">
+                    <img src="./assets/images/profiles/default.jpg" alt="Profile Image"/>
+                </div>
+            </a>
+            <ul>
+                <li><a href="account-settings.php">Account Settings</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
